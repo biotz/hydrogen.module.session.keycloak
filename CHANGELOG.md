6e4060e0a5e0e82c86d82e6d85fbc29c53830d3c
Damian Hryniewicz <damian@magnet.coop>
2019-06-17 19:10:21 +0200

    Add config for BE token verification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 
+## UNRELEASED
+### Added
+- Config necessary for backend token verification
+
 ## [0.1.0] - 2019-06-10
 
 ### Added
 - Initial version!
 
 [0.1.0]: https://github.com/magnetcoop/hydrogen.module.session.keycloak/releases/tag/v0.1.0
+[Unreleased]: https://github.com/magnetcoop/hydrogen.module.session.keycloak/compare/v0.1.0...HEAD/
--- src/hydrogen/module/session/keycloak.clj
+++ src/hydrogen/module/session/keycloak.clj
@@ -11,17 +11,28 @@
    {:keycloak
     {:realm (env/env '["KEYCLOAK_REALM" Str])
      :url (env/env '["KEYCLOAK_URL" Str])
-     :clientId (env/env '["KEYCLOAK_CLIENT_ID" Str])}}})
+     :clientId (env/env '["KEYCLOAK_CLIENT_ID" Str])}}
+
+   :magnet.buddy-auth/jwt-oidc
+   {:claims
+    {:iss (env/env '["OIDC_ISSUER_URL" Str])
+     :aud (env/env '["OIDC_AUDIENCE" Str])}
+    :jwks-uri (env/env '["OIDC_JWKS_URI" Str])}
+
+   :duct.middleware.buddy/authentication
+   {:backend :token
+    :token-name "Bearer"
+    :authfn (ig/ref :magnet.buddy-auth/jwt-oidc)}})
 
 (defn- session-config [options project-ns]
   (cond->
    (session-config-base project-ns)
 
     (:add-example-api? options)
-    (assoc (keyword (str project-ns ".api/example")) {})))
+    (assoc (keyword (str project-ns ".api/example"))
+           {:auth-middleware (ig/ref :duct.middleware.buddy/authentication)})))
 
 (defmethod ig/init-key :hydrogen.module/session.keycloak [_ options]
   (fn [config]
     (let [project-ns (project-ns config options)]
       (core/merge-configs config (session-config options project-ns)))))
-
